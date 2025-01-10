4c3d6b08de8808134802cd60578ac4bde4051db0
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2025-01-10 14:08:46 +0530

    fixed image upload error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-images:{
-  domains:[
-    "utfs.io"
-  ]
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: '9j56yz4me3.ufs.sh',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
