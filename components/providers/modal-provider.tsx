15b05850091f180f2294f7df5cee4260534e2e17
Smit Shah <smitshah084@gmail.com>
2025-02-09 14:43:31 +0530

    server channels list and edit channel

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -8,6 +8,8 @@ import { MembersModal } from "@/components/models/members-modal";
 import { CreateChannelModal } from "../models/create-channel-modal";
 import { LeaveServerModal } from "../models/leave-server-modal";
 import { DeleteServerModal } from "../models/delete-server-modal";
+import { DeleteChannelModal } from "../models/delete-channel-modal";
+import { EditChannelModal } from "../models/edit-channel-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -30,7 +32,9 @@ export const ModalProvider = () => {
             <CreateChannelModal/>
             <LeaveServerModal/>
             <DeleteServerModal/>
-        </div>
+            <DeleteChannelModal/>
+            <EditChannelModal/>
+            </div>
     );
 
 }
